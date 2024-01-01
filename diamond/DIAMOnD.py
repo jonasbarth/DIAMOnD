33632d276a547fad94a721373c52fd251906c4d9
jonasbarth <jonas.barth.95@gmail.com>
2024-01-01 19:13:51 +0100

    convert args to list

--- diamond/DIAMOnD.py
+++ diamond/DIAMOnD.py
@@ -418,7 +418,7 @@ def DIAMOnD(G_original, seed_genes, max_number_of_added_nodes, alpha, outfile=No
 
 def diamond(*args):
     # add dummy element at the front
-    args = [0] + args
+    args = [0] + list(args)
     network_edgelist_file, seeds_file, max_number_of_added_nodes, alpha, outfile_name = check_input_style(args)
 
     # read the network and the seed genes:
